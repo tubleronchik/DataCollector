f5eb82295f16d1a537f3443e9b42b9dd658b7c4d
tubleronchik <m.bystramovich.gmail.com>
2020-03-22 02:11:43 +0300

    updated models.py

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Data(db.Model):
+    #create a table
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    age = db.Column(db.Integer)
+    country = db.Column(db.String)
+    db.create_all()
+
+    def __init__(self, age, country): #constructor for a class
+        self.age = age
+        self.country = country
+
